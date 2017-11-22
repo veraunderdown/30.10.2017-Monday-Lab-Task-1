8b7cc29880583a56398ecc5e811c80fa4e67f91f
Oleg Gorecan <rvo@btinternet.com>
2017-11-22 16:56:48 +0000

    Add project files.

---
+++ 30.10.2017 Monday Lab Task 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30._10._2017_Monday_Lab_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
